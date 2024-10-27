31a2ba86a55c6437a71f5781442f56f35dd34be8
Luciano Machado <lucianoomachados@gmail.com>
2024-10-27 11:10:09 -0300

    Estilizando as tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+
+//Form
+import { FaPlus } from "react-icons/fa"
+
+//Tarefas
+import { FaEdit, FaWindowClose } from "react-icons/fa"
+
 import "./Main.css";
 
 export default class Main extends Component {
   state = {
     newTask: "",
+    tasks: [
+      'Lavar a louca',
+      'Estudar React',
+      'Ler um livro',
+    ],
   };
 
   handleChange = (event) => {
@@ -12,15 +24,35 @@ export default class Main extends Component {
     });
   }
   render() {
-    const { newTask } = this.state;
+    const { newTask, tasks } = this.state; //Desestruturação do metodo state
 
     return (
     <div className="main">
       <h1>Lista de Tarefas</h1>
-      <form action="#">
-        <input onChange={this.handleChange} type="text" placeholder="Tarefa"/>
-        <button type="submit">Adicionar</button>
+
+      <form action="#" className="form">
+        <input
+        onChange={this.handleChange}
+        type="text"
+        value={newTask}
+        />
+        <button type="submit">
+          <FaPlus />
+        </button>
       </form>
+
+      {/* Renderização condicional e não podemos esquecer da key*/}
+      <ul className="tasks">
+        {tasks.map((task) => (
+          <li key={ task }>{ task }
+            <div>
+              <FaEdit className="edit" />
+              <FaWindowClose className="delete" />
+            </div>
+          </li>
+        ))}
+      </ul>
+
     </div>
     );
   }
