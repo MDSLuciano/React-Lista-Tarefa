ebd757792440131139ec02ff20f636d3a69e0859
Luciano Machado <lucianoomachados@gmail.com>
2024-10-27 15:39:30 -0300

    Salvando as tarefas no localStorage

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,11 +12,32 @@ export default class Main extends Component {
   state = {
     newTask: "",
     tasks: [],
+    index: -1,
   };
+
+  componentDidMount() {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if (!tasks) return;
+
+    this.setState({ tasks });
+  }
+
+  //Persiste as tarefas, se tiver alteração ele persiste os dados no localStorage
+  componentDidUpdate(prevProps, prevState) {
+    const { tasks } = this.state;
+
+    if (tasks === prevState.tasks) return;
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+
 
   handleSubmit = (event) => {
     event.preventDefault();
-    let { newTask, tasks } = this.state;
+    const { tasks, index } = this.state;
+    let { newTask } = this.state;
     newTask = newTask.trim();
 
     //Validação se existe uma tarefa vazia
@@ -24,13 +45,24 @@ export default class Main extends Component {
 
     //Validação se a tarefa ja existe
     if (tasks.indexOf(newTask) > -1) return;//Se tiver retorna
-    const newTasks = [...tasks, newTask];//Cria uma nova tarefa
+    const newTasks = [...tasks];//Cria uma nova tarefa
 
-    this.setState({
-      tasks: newTasks,
-      newTask: "",//
-    });
+    if (index === -1) {
+      this.setState({
+        tasks: [...newTasks, newTask],//Primeiro parametro são as tarefas anteriores, e o segundo parametro e a nova tarefa
+        newTask: "",//Limpa o input
+      });
+    }else {
+      newTasks[index] = newTask;//Edita uma tarefa
+
+      this.setState({
+        tasks: newTasks,
+        newTask: "",
+        index: -1
+      });
+    };
   }
+
 
   handleChange = (event) => {
     this.setState({
@@ -39,7 +71,13 @@ export default class Main extends Component {
   }
 
   handleEdit = (event, index) => {
-    console.log('Editando tarefa', index);
+    const { tasks } = this.state;//Desestruturação do metodo state
+
+    //Edita uma tarefa
+    this.setState({
+      index,
+      newTask: tasks[index]
+    })
 
   }
 
